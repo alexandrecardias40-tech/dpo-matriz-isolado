c94723d19bccec25ffe0b1488dc896c18c786f6d
CLIENTE <cliente@MacBook-Air-de-CLIENTE.local>
2026-06-11 12:40:57 -0300

    Restore LandingPage for isolated site

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,16 +3,17 @@ import { useHashLocation } from "wouter/use-hash-location";
 import App from "./App";
 import GraficosPage from "./GraficosPage";
 import ComparativosPage from "./ComparativosPage";
+import LandingPage from "./LandingPage";
 
 export default function Router() {
   return (
     <WouterRouter hook={useHashLocation}>
       <Switch>
-        <Route path="/" component={App} />
+        <Route path="/" component={LandingPage} />
         <Route path="/dashboard" component={App} />
         <Route path="/graficos" component={GraficosPage} />
         <Route path="/comparisons" component={ComparativosPage} />
-        <Route component={App} />
+        <Route component={LandingPage} />
       </Switch>
     </WouterRouter>
   );
